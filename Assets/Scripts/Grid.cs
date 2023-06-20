82b6484c93195bf37b2ab5f91a9bff5bb16ead08
Leandro (Cerberus1746) Benedet Garcia <cerberus1746@gmail.com>
2023-06-20 19:10:36 -0300

    Done the following changes:
    * Turned on code coverage
    * Tweaked downloader test
    * Started Grid construction

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,18 +4,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using Newtonsoft.Json;
+using System;
 
 namespace Drone
 {
     public struct Tile
     {
         public Vector2Int index;
+        public string letterCoordinate;
         public Dictionary<Tile, float> neighbors;
     }
 
     public sealed class Grid
     {
-        Dictionary<char, int> letterToInt = new(){
+        public string url = "https://mocki.io/v1/10404696-fd43-4481-a7ed-f9369073252f";
+        Dictionary<char, int> _letterToInt = new(){
             {'A', 0},
             {'B', 1},
             {'C', 2},
@@ -25,15 +28,57 @@ namespace Drone
             {'H', 6}
         };
 
-        Tile[] allTiles;
+        Dictionary<string, Tile> _allTiles;
 
         Dictionary<string, Dictionary<string, float>> DownloadAndParseGridData()
         {
-            using (var wc = new WebClient())
+            using var wc = new WebClient();
+            var json = wc.DownloadString(url);
+            return JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, float>>>(json);
+        }
+
+        void GenerateGrid()
+        {
+            _allTiles = new();
+            var parsed = DownloadAndParseGridData();
+
+            foreach (var currParsedTile in parsed)
             {
-                var json = wc.DownloadString("https://mocki.io/v1/10404696-fd43-4481-a7ed-f9369073252f");
-                return JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, float>>>(json);
+                var neighbors = currParsedTile.Value;
+                var neighborLen = neighbors.Count;
+
+                Tile currTile = GetOrCreateTile(currParsedTile.Key);
+                foreach (var currNeighbor in neighbors)
+                {
+                    var converted = GetOrCreateTile(currNeighbor.Key);
+
+                    // For some reason the distances are different back and forth
+                    // Otherwise I would add each other as a neighbor here.
+                    currTile.neighbors[converted] = currNeighbor.Value;
+                }
             }
+        }
+
+        Tile GetOrCreateTile(string key)
+        {
+            if (_allTiles.ContainsKey(key)) return _allTiles[key];
+
+            var createdTile = new Tile
+            {
+                index = FromLetterNumberToVector(key),
+                letterCoordinate = key,
+                neighbors = new()
+            };
+            _allTiles[key] = createdTile;
+            return createdTile;
+        }
+
+        /// <summary>Transform letter and number coordinate system to a Vector to be used by Unity later</summary>
+        Vector2Int FromLetterNumberToVector(string letterAndNumber)
+        {
+            var firstNumber = _letterToInt[letterAndNumber[0]];
+            var secondNumber = (int)letterAndNumber[1];
+            return new(firstNumber, secondNumber);
         }
     }
 }
--- Assets/Tests/TestGrid.cs
+++ Assets/Tests/TestGrid.cs
@@ -9,14 +9,27 @@ using Drone;
 
 public class TestGrid
 {
-    // A Test behaves as an ordinary method
     [Test]
-    public void TestDownloadAndParse()
+    public void TestDownloaderAndParser()
     {
         var grid = new Drone.Grid();
         var parsed = Utils.CallPrivateMethod(grid, "DownloadAndParseGridData", new object[0]) as Dictionary<string, Dictionary<string, float>>;
-        foreach(var currTile in parsed)
-            foreach(var neighbor in currTile.Value)
-                Debug.Log($"Neighbors of {currTile.Key}: {neighbor.Key}, {neighbor.Value}");
+
+        foreach (var currTile in parsed)
+        {
+            var coordinate = currTile.Key;
+
+            Assert.True(char.IsLetter(coordinate[0]));
+            Assert.True(char.IsNumber(coordinate[1]));
+
+            foreach (var neighbor in currTile.Value)
+            {
+                var neighborCoordinate = neighbor.Key;
+
+                // no need to test for the times because the parser automatically raises an error if it were to be invalid
+                Assert.True(char.IsLetter(neighborCoordinate[0]));
+                Assert.True(char.IsNumber(neighborCoordinate[1]));
+            }
+        }
     }
 }
